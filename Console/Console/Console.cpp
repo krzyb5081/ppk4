e219e52ce935db44524656222fa51a349c9383d9
Krzysztof <37555471+krzyb5081@users.noreply.github.com>
2022-11-02 09:41:59 +0100

    Add files via upload

---
+++ Console/Console/Console.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+void generation(std::vector<std::vector<std::vector<double>>>& wektorWektorowWektorowDouble) {
+	double liczba = 0.2;
+
+	for (int i = 0; i < 10; i++) {
+		std::cout << "i: " << i << std::endl;
+
+		std::vector<std::vector<double>> wektorWektorowDouble;
+
+		for (int j = 0; j < 10; j++) {
+			std::cout << "j: " << j << std::endl;
+
+			std::vector<double> wektorDouble;
+
+			for (int k = 0; k < 10; k++) {
+				std::cout << "k: " << k << std::endl;
+
+				wektorDouble.push_back(liczba *= 2);
+
+			}
+			wektorWektorowDouble.push_back(wektorDouble);
+
+		}
+		wektorWektorowWektorowDouble.push_back(wektorWektorowDouble);
+
+	}
+}
+
+void display(std::vector<std::vector<std::vector<double>>>& wektorWektorowWektorowDouble) {
+	
+}
+
+int main() {
+	std::vector<std::vector<std::vector<double>>> wektorWektorowWektorowDouble;
+
+	generation(wektorWektorowWektorowDouble);
+
+	display(wektorWektorowWektorowDouble);
+
+}
